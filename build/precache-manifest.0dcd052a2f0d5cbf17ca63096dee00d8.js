1a94eddbc5dd7838edd82e9bb878edd9daf3cd62
someone <someone@example.com>
2018-12-05 11:40:21 -0600

    first build commit

---
+++ build/precache-manifest.0dcd052a2f0d5cbf17ca63096dee00d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "692a2c5f6c24deda291e",
+    "url": "/react_todos/static/js/runtime~main.692a2c5f.js"
+  },
+  {
+    "revision": "3a8d23f44dad449b4b94",
+    "url": "/react_todos/static/js/main.3a8d23f4.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/react_todos/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "3a8d23f44dad449b4b94",
+    "url": "/react_todos/static/css/main.b51effe9.chunk.css"
+  },
+  {
+    "revision": "381df7ee39bcccc2a5aa8c83a73fa0b9",
+    "url": "/react_todos/index.html"
+  }
+];
